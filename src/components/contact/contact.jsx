1192aae0dcf8cb701d28fc3a254268f578bbeabe
Gabriel Rodrigues <gabriel.int96@gmail.com>
2022-06-09 20:20:00 -0300

    adds input handler

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SectionsHeader } from '../sectionsHeader/sectionsHeader';
 import { ContactContainer, FormContainer } from './styles';
 
 function Contact(props) {
+    const [input, setInput] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    const handleChange = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        }
+        )
+    }
     return (
         <ContactContainer>
             <SectionsHeader h1={'Contato'} h2={'Vamos conversar?'} />
             <FormContainer>
                 <form>
                     <label>Nome
-                        <input type='text' name='name' />
+                        <input type='text' name='name' onChange={handleChange} value={input.name} />
                     </label>
                     <label>E-mail
-                        <input type='email' name='email' />
+                        <input type='email' name='email' onChange={handleChange} value={input.email} />
                     </label>
                     <label>Mensagem
-                        <textarea name='message' />
+                        <textarea name='message' onChange={handleChange} value={input.message} />
                     </label>
                 </form>
             </FormContainer>
